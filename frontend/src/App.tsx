0d12b4df40d0a8de180412b4ef90fb24cccf66f8
ezanaproton <proton78978@proton.me>
2025-05-27 15:40:55 +0300

    booking functionality added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  // Navigate,
+  Navigate,
 } from 'react-router-dom'
 import SignIn from './pages/SignIn.tsx'
 import AddHotel from './pages/AddHotel.tsx'
@@ -14,6 +14,8 @@ import MyHotels from './pages/MyHotels.tsx'
 import EditHotel from './pages/EditHotel.tsx'
 import Search from './pages/Search.tsx'
 import Detail from './pages/Details.tsx'
+import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -23,15 +25,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><p><Search/></p></Layout>} />
         <Route path="/search" element={<Layout><Search></Search></Layout>} />
-        <Route path="*" element={<div className='text-center h-screen text-3xl align-items justify-center'>Coming Soon</div>} />
+        <Route path="*" element={<Navigate to="/" />}/>
         <Route path="/register" element={<Layout><Register></Register></Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn></SignIn></Layout>}></Route>
         <Route path="/detail/:hotelId" element={<Layout><Detail /></Layout>}/>
         {isLoggedIn &&
           (<>
+            <Route path="/hotel/:hotelId/booking" element={<Layout><Booking /></Layout>}/>
             <Route path="/my-hotels" element={<Layout><MyHotels></MyHotels></Layout>}></Route>
             <Route path="/add-hotel" element={<Layout><AddHotel></AddHotel></Layout>}></Route>
             <Route path="/edit-hotel/:hotelId" element={<Layout><EditHotel></EditHotel></Layout>}></Route>
+            <Route path="/my-bookings" element={<Layout><MyBookings /></Layout>}/>
           </>)}
       </Routes>
     </Router>
